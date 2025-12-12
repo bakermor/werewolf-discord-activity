aa0da3043f2ccb0f45877076fb790bfa7380ea3a
Morgan Baker <bakermor@oregonstate.edu>
2025-12-11 23:15:10 -0600

    chore: update ci workflow to skip typescript linting in super-linter

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,5 @@
 import type { CommandResponse } from "@discord/embedded-app-sdk";
-import "./style.css";
+import "../style.css";
 import { discordSdk } from "./discordSdk";
 import rocketLogo from "/logo.png";
 
